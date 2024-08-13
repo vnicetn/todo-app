0323c83e0baf9a828471664baabd5ae9b4ed425b
vnicetn <vincentandpancakes@gmail.com>
2024-08-13 16:39:38 +0500

    written handlers, todo architecture of lists and items, as well as created a structure for users, project architecture is gewd

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/vnicetn/todo-app"
+	"github.com/vnicetn/todo-app/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
